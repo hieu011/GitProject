775c7ad949cec01e760f96643fafeaf32caf437e
hieu011 <129260843+hieu011@users.noreply.github.com>
2023-04-07 10:26:54 +0700

    kbsdbien

---
+++ module1/javascript/kbsdbien.js
@@ -0,0 +1,9 @@
+var a = prompt('nhap a');
+var b = prompt('nhap b');
+a = parseInt (a);
+b = parseInt (b);
+if (a % b ==0) {
+    alert(a+'là bội số của'+b);
+} else {
+    alert (a+' không là bội số của'+b);
+}
